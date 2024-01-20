7e67d262bea1e890d741eaf00198e424d52d2f88
ShakirKadirov <shakirkadirov1@gmail.com>
2024-01-20 22:38:55 +0600

    добавлено два Label name surname и разместил на LabelStack

--- Nav/MainViewController.swift
+++ Nav/MainViewController.swift
@@ -15,12 +15,33 @@ class MainViewController: UIViewController {
         $0.layer.cornerRadius = $0.frame.height / 2
         return $0
     }(UIImageView(frame: CGRect(x: 100, y: 100, width: 100, height: 100)))
+    
+    lazy var labelStack: UIStackView = {
+        $0.axis = .horizontal
+        $0.spacing = 20
+        $0.alignment = .center
+        $0.addArrangedSubview(nameLbl)
+        $0.addArrangedSubview(surNameLbl)
+        return $0
+    }(UIStackView(frame: CGRect(x: 100, y: profileImg.bounds.height + 120, width: profileImg.frame.width + 100, height: 20)))
 
+    lazy var nameLbl: UILabel = {
+        $0.text = "Имя"
+        $0.font = UIFont.boldSystemFont(ofSize: 20)
+        return $0
+    }(UILabel())
+    
+    lazy var surNameLbl: UILabel = {
+        $0.text = "Фамилия"
+        $0.font = UIFont.boldSystemFont(ofSize: 20)
+        return $0
+    }(UILabel())
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         
         view.addSubview(profileImg)
+        view.addSubview(labelStack)
     }
 
 
