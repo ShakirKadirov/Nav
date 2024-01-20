7fc4509238514c9ec485d7f9dcdbc53145708dc6
ShakirKadirov <shakirkadirov1@gmail.com>
2024-01-20 22:18:44 +0600

    К Assets добавлено mountainImage.jpg  и на экран добавлено этот изобрежение как profileImg

--- Nav/MainViewController.swift
+++ Nav/MainViewController.swift
@@ -8,11 +8,19 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    lazy var profileImg: UIImageView = {
+        $0.image = UIImage(named: "mountainImage.jpg")
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = $0.frame.height / 2
+        return $0
+    }(UIImageView(frame: CGRect(x: 100, y: 100, width: 100, height: 100)))
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         
-        view.backgroundColor = .systemBlue
+        view.addSubview(profileImg)
     }
 
 
